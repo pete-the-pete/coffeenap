623988a0de1d5344cb88d3c72368d06ad0dfb984
pete_the_pete <pliljegr@gmail.com>
2023-02-05 22:00:47 -0800

    Removing css for backround. Add it inline like a noob

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en" style={{
-      backgroundImage: `url(bg/background.jpg)`
+      backgroundImage: `url(bg/background.jpg) no-repeat center fixed`
     }}>
       <Head />
       <body>
