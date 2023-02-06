79b4dbcb848e5cf1a3940c8f6443fece40badf61
pete_the_pete <pliljegr@gmail.com>
2023-02-05 21:57:36 -0800

    Background shows up. More stuff happens

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  assetPrefix: '.',
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
